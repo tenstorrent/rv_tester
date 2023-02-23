91890047246fba342075733bd17d2015f69e506a
Pravin Tavagad <ptavagad@aus-rv-l-3.local.tenstorrent.com>
2023-02-23 01:18:26 -0600

    add empty definitions

--- sysmod/null_dev/null_dev.cpp
+++ sysmod/null_dev/null_dev.cpp
@@ -2,27 +2,20 @@
 #include "null_dev.h"
 
 
-// void null_dev::write(uint64_t addr, size_t length, const data_t& data, const strb_t& strb,
-//                 cbs_t& cbs) {
-//   if (not has_addr(addr))
-//     return;
+ void null_dev::write(uint64_t addr, size_t length, const data_t& data, const strb_t& strb,
+                 cbs_t& cbs) {
+   if (not has_addr(addr))
+     return;
+   return;
+ }
 
-//   for (size_t i = 0; i < length; i++) {
-//     if (strb[i]) {
-//       m_.write(addr + i, 1, &data[i]);
-//     }
-//   }
-//   return;
-// }
+ void null_dev::read(uint64_t addr, size_t length, data_t& data, cbs_t& cbs) {
+   if (not has_addr(addr))
+     return;
 
-// void null_dev::read(uint64_t addr, size_t length, data_t& data, cbs_t& cbs) {
-//   if (not has_addr(addr))
-//     return;
 
-//   m_.read(addr, length, data.data());
-
-//   return;
-// }
+   return;
+ }
 
 
 // bool null_dev::init_elf(const std::string& path) {
