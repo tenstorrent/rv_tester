860d46ce90ea389765cacde964fe6a36ae7589b4
Varun Koyyalagunta <vkoyyalagunta@tenstorrent.com>
2023-04-14 16:18:16 -0500

    chore: verilator use opt build

--- sw_testbench/testlists/smoke.py
+++ sw_testbench/testlists/smoke.py
@@ -2,4 +2,4 @@
 
 if __name__ == '__main__':
     print("cc_test_generator --name infinite -s //sw_testbench/testlists:infinite.S")
-    print("risc_p_cores_test_executor --name infinite_verilator --test infinite --simulator verilator --dbg dbg --tb sw_testbench:sw_testbench --run-arg +nostandalone +eot=max_instr +max_instr=1")
+    print("risc_p_cores_test_executor --name infinite_verilator --test infinite --simulator verilator --tb sw_testbench:sw_testbench --run-arg +nostandalone +eot=max_instr +max_instr=1")
