6e90e6a3ec96d9a2d83985fb4cd4c60cf6d2c358
Jiahan Zhang <jzhang@tenstorrent.com>
2023-06-14 14:10:56 -0500

    fix cosim run

--- cosim/whisper_cov/arch_sample.cpp
+++ cosim/whisper_cov/arch_sample.cpp
@@ -8,14 +8,22 @@
 #include "svdpi.h"
 #include "src/cov_sample/cov_sample_interface.hpp"
 #include "cvm/plusargs.hpp"
+#include "cvm/registry.hpp"
 
 DECLARE_bool(cov);
 
 using namespace ArchCov;
 covSampleInterface covSampleInterface_;
+svScope scope_;
 
 extern "C" void sample_sv(const cp_pkt*);
 
-extern "C" void trace(arch_t& table) {
-  covSampleInterface_.sample_packets(table);
+extern "C" void trace(arch_t table) {
+  cvm::registry::callbacks.push(
+      scope_,
+      [=]() { covSampleInterface_.sample_packets(const_cast<ArchCov::arch_t&>(table)); });
+}
+
+extern "C" void archcov_set_scope() {
+  scope_ = svGetScope();
 }
