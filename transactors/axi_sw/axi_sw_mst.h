7ff8d9d074f46bee3f02f6ad2d554b1d75016106
Jiahan Zhang <jzhang@tenstorrent.com>
2023-07-31 17:56:33 +0000

    Checkin script: fully resize data/strb vector for master. fix strb

--- transactors/axi_sw/axi_sw_mst.h
+++ transactors/axi_sw/axi_sw_mst.h
@@ -59,6 +59,7 @@ class axi_sw_mst {
         cvm::topology::loc_t loc_;
         size_t id_width_;
         size_t data_width_;
+        size_t strb_width_;
 
         const size_t ar_q_max_, ar_q_ptr_max_;
         const size_t aw_q_max_, aw_q_ptr_max_;
