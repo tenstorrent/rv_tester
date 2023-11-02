791bbfbbaf2cdb6836a65b10d83975e886424861
Pravin Tavagad <ptavagad@aus-rv-l-3.local.tenstorrent.com>
2023-11-02 00:44:11 -0500

    CVM logs and enhancements

--- sysmod/trickbox/trickbox.cpp
+++ sysmod/trickbox/trickbox.cpp
@@ -48,7 +48,6 @@ void trickbox::read(const transactor::read_t& r, data_t& data) {
 
   for (auto& d : subdevices_) {
     d->read_dev(addr,length,data);
-    cvm::log ("$$$$$$$$$$$$ Reading From {} $$$$$$$$$$$$$$$$\n",d->tag());
     cvm::log (cvm::HIGH,"TRICKBOX READ::::: ADDR:{:#x} \n",addr);
     cvm::log (cvm::HIGH,"TRICKBOX READ::::: DATA byte 0:{:#x} \n",(uint32_t)data[0]);
     cvm::log (cvm::HIGH,"TRICKBOX READ::::: DATA byte 1:{:#x} \n",(uint32_t)data[1]);
