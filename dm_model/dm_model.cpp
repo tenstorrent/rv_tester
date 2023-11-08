a4ba85be4a26ab42ce5718d24b29fe016a577ca6
travi <travi@tenstorrent.com>
2023-11-08 02:36:51 -0600

    Aarsize update

--- dm_model/dm_model.cpp
+++ dm_model/dm_model.cpp
@@ -592,7 +592,7 @@ bool debug_module_t::perform_abstract_command()
     bool transfer = get_field(command, AC_ACCESS_REGISTER_TRANSFER);
     bool unsupported_command = false;
 
-    if (size > 4)
+    if (size > 3)
     {
       abstractcs.cmderr = CMDERR_NOTSUP;
       return true;
