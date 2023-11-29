fd890569b7d700b422f46b3b76b1b8c975471802
Mansoor Anees <manees@tenstorrent.com>
2023-11-29 04:18:05 -0600

    Change imsic source from axi mst to mst2

--- rv_tester.sv
+++ rv_tester.sv
@@ -303,7 +303,7 @@ import rv_tester_params::*;
           .mcmi_write(mcmi_write[NWRITES_CUMSUM[c] +: NWRITES[c]]),
           .mcmi_bypass(mcmi_bypass[NBYPASSES_CUMSUM[c] +: NBYPASSES[c]]),
           .wired_interrupt(interrupt_pend[c]),
-          .imsic_interrupt(axi_req_mst[1]),
+          .imsic_interrupt(axi_req_mst2[0]),
           .debug_mode(debug_mode[c]),
           .terminate(cosim_terminate[c]),
           `RV_TESTER_TRANSACTIONS_COSIM_SOURCE_PORTS(1, c, 0)
