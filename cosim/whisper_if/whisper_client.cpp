b14e25c5b68f6840fd09d6eb4005f3a2a79fab53
Mansoor Anees <manees@tenstorrent.com>
2023-12-08 04:58:55 -0600

    Fix csr check related bugs
    - Mask zicsr rtl updates with whisper json mask
    - Mask hw csr rtl updates with whisper json poke mask
    - Modify csr rtl data for special cases - like pmacfg
    - Init cac csrs with reset values from whiser json
    - Collect all csr updates from a ucode instruction/routine and send with the last uop for checking
    - Disable custom csr checks by default and maintain a supported custom list - only pmacfg for now
    - Refactor csr writes to use update_csr method
    - Modify csr mismatch signature to reflect names instead of addresses

--- cosim/whisper_if/whisper_client.cpp
+++ cosim/whisper_if/whisper_client.cpp
@@ -198,7 +198,7 @@ whisperClient<URV>::whisperPeek(int hart, char resource, uint64_t addr, uint64_t
 template <typename URV>
 bool
 whisperClient<URV>::whisperPeekCsr(int hart, uint64_t addr, uint64_t& value, uint64_t& mask,
-         bool& valid)
+         uint64_t& poke_mask, bool& valid)
 {
   req.hart = hart;
   req.type = WhisperMessageType::Peek;
@@ -211,6 +211,7 @@ whisperClient<URV>::whisperPeekCsr(int hart, uint64_t addr, uint64_t& value, uin
   valid = reply.type != WhisperMessageType::Invalid;
   value = reply.value;
   mask = reply.address;
+  poke_mask = reply.time;
   return true;
 }
 
