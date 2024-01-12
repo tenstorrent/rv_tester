c8008147acf06ebcb5b25d37cab038258bd95324
Mansoor Anees <manees@tenstorrent.com>
2024-01-12 03:11:34 -0500

    Disable mcm before post-terminate step to avoid asserts

--- cosim/whisper_if/whisper_client.h
+++ cosim/whisper_if/whisper_client.h
@@ -28,6 +28,7 @@ class whisperClient {
 
     int whisperConnect(uint16_t ncores);
     void whisperDisconnect();
+    void whisperDisableMcm();
     bool whisperStep(int hart, uint64_t time, uint64_t instrTag, uint64_t& pc, uint32_t& instruction, unsigned& changeCount, std::string& disasm, uint32_t& privMode, uint32_t& fpFlags, bool& hasTrap, bool& hasStop);
     bool whisperSimpleStep(int hart, uint64_t& pc, uint32_t& instruction, unsigned& changeCount);
     bool whisperChange(int hart, uint32_t& resource, uint64_t& addr, uint64_t& value, bool& valid);
