9e0bd2d203e6970afe32626287fa6f9919880e19
Varun Koyyalagunta <vkoyyalagunta@tenstorrent.com>
2024-01-20 18:21:38 -0600

    chore: zebu clock

--- rv_tester_clkgen.sv
+++ rv_tester_clkgen.sv
@@ -4,15 +4,19 @@ module rv_tester_clkgen #(
     output logic clk
 );
 
-    int CLOCK_PERIOD_PS = 1000000 / CLOCK_FREQ_MHZ;
+    localparam int CLOCK_PERIOD_PS = 1000000 / CLOCK_FREQ_MHZ;
 
     `ifdef IXCOM_COMPILE
         IXCclkgen #(CLOCK_PERIOD_PS/2) uclk (clk);
+    `else
+    `ifdef ZEBU_CLOCK_DELAY_PORT
+        clockDelayPort #(CLOCK_PERIOD_PS/2,CLOCK_PERIOD_PS/2,0) ClockPort(clk);
     `else
         initial begin
             clk = '0;
             forever  #(CLOCK_PERIOD_PS*1ps/2) clk = ~clk;
         end
+    `endif
     `endif
 
 endmodule
