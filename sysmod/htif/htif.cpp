ae9bea149edd636804cf249e1d2bfa288d8e2e2a
Varun Koyyalagunta <vkoyyalagunta@tenstorrent.com>
2024-04-04 17:01:08 -0500

    feat: WIP try to get terminal input

--- sysmod/htif/htif.cpp
+++ sysmod/htif/htif.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cassert>
 #include <unistd.h>
+#include <pty.h>
 #include <termios.h>
 #include <poll.h>
 #include "htif.h"
@@ -10,6 +11,65 @@
 #include "cvm/plusargs.hpp"
 
 DEFINE_bool(htif_flip, false, "Reverse the htif tohost/fromhost address order");
+DEFINE_bool(pty, false, "Use a pseudo-terminal for the HTIF console");
+
+// https://stackoverflow.com/a/45675259
+htif::pty::pty()
+{
+  if (!FLAGS_pty)
+  {
+    return;
+  }
+
+  struct termios tty;
+  tty.c_iflag = (tcflag_t) 0;
+  tty.c_lflag = (tcflag_t) 0;
+  tty.c_cflag = CS8;
+  tty.c_oflag = (tcflag_t) 0;
+
+  char name[256];
+  auto e = openpty(&master, &slave, name, &tty, nullptr);
+  if(0 > e) {
+    master = -1;
+    slave = -1;
+    cvm::log(cvm::ERROR, "Error: {}\n", std::strerror(errno));
+  }
+
+  cvm::log(cvm::NONE, "HTIF PTY: {}\n", name);
+}
+
+htif::pty::~pty()
+{
+  if (slave != -1)
+    close(slave);
+  if (master != -1)
+    close(master);
+}
+
+int htif::pty::write(char c) {
+  if (master == -1)
+    return -1;
+
+  return ::write(master, &c, 1);
+}
+
+int htif::pty::read() {
+  if (master == -1)
+    return -1;
+
+  char c;
+  int r = ::read(master, &c, sizeof(c));
+  if (r == 1)
+    return c;
+
+  if (r == -1) {
+    cvm::log(cvm::ERROR, "Error: pty read: unexpected fail on read, errno={}\n", strerror(errno));
+    return -1;
+  }
+
+  cvm::log(cvm::ERROR, "Error: pty read: unexpected fail on read\n");
+  return -1;
+}
 
 htif::htif(const std::string& tag, uint64_t addr, cvm::topology::loc_t loc)
   : device(tag, addr, 16 /* size */, loc, &htif::write, &htif::read, this), to_(0), from_(0)
@@ -120,13 +180,17 @@ htif::write(const transactor::write_t& w)
 	  char c = payload;
 	  if (c)
 	    {
+	      pty_.write(c);
 	      putchar(c);
 	      fflush(stdout);
 	    }
 	}
       else if (cmd == 0)
 	{
-	  int ch = readCharNonBlocking(fileno(stdin));
+	  int ch;
+	  ch = pty_.read();
+	  if (ch < 0)
+	    ch = readCharNonBlocking(fileno(stdin));
 	  if (ch > 0)
 	    from_ = ((payload >> 48) << 48) | uint64_t(ch);
 	}
