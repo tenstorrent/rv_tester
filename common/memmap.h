97e02850909831813566530108117462b10efb1c
Pravin Tavagad <ptavagad@tenstorrent.com>
2024-04-05 08:34:56 +0000

    Update common/memmap.h

--- common/memmap.h
+++ common/memmap.h
@@ -1,1 +1,25 @@
+#pragma once
 
+#include <map>
+#include <string>
+#include <cstdint>
+
+namespace memmap {
+
+  struct memmap_entry_t {
+      std::string base_str;
+      uint64_t base;
+      uint64_t size;
+      std::string   type;
+      std::string   tag ;
+      uint64_t end ;
+  };
+
+  // ordered so when we iterate to fill SV and create devices it will be deterministic
+  using memmap_t = std::map<std::string, memmap_entry_t>;
+
+  extern memmap_t m;
+
+  void get(memmap_t& memmap);
+  void parse();
+}
