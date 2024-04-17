e91b8da713600a4d333adcbb212771de905ab569
Abdultaiyeb V <abduv@aus-rv-l-9.local.tenstorrent.com>
2024-04-17 15:11:33 -0500

    removing hstatus from csr_write_check

--- cosim/bridge/bridge.cpp
+++ cosim/bridge/bridge.cpp
@@ -82,7 +82,7 @@ bridge::bridge(int num_harts, int xlen, int vlen, cvm::topology::loc_t loc, unsi
       //"htval","mtval2", // RVDE-10043
       "mtinst","htinst", // RVDE-10005
       "vstart","vxsat","vxrm","vcsr", // Unimplemented
-      "sstatus","mstatus","mie","hie","vsie","sie", // RVDE-11840
+      "sstatus","mstatus","hstatus","mie","hie","vsie","sie", // RVDE-11840
       "tselect","tdata1","tdata2","tdata3","mcontext", // Unimplemented: RVDE-7518
       "fflags","fcsr", // Unimplemented
       "menvcfg","senvcfg", // FIXME: pointer masking change
