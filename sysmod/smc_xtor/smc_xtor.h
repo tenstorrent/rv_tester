c42a4dbbcca4789f6e507a40e0f242245862c6b6
Pravin Tavagad <ptavagad@tenstorrent.com>
2024-04-17 01:08:17 +0000

    Update sysmod/smc_xtor/smc_xtor.cpp, sysmod/smc_xtor/smc_xtor.h

--- sysmod/smc_xtor/smc_xtor.h
+++ sysmod/smc_xtor/smc_xtor.h
@@ -103,7 +103,9 @@ class smc_xtor : public device {
         
         virtual void tick(uint64_t) override
         {
-
+           if(!FLAGS_smc_en)
+            return;
+            
             cvm::log(cvm::FULL, "[SMC] tick {:#X} \n",cnt_tick);
             if(in_boot_seq && ( cnt_tick > uint32_t(FLAGS_smc_reset_seq_start_ticks))){
             cvm::log(cvm::FULL, "[SMC] IN BOOT SEQ {} reset complition {} \n",in_boot_seq,reset_completion);
