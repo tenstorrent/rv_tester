ebbaf7ab72dd1ad0e5ac09ec524cd5321e7cf2be
Pravin Tavagad <ptavagad@aus-rv-l-8.local.tenstorrent.com>
2024-08-02 14:51:59 -0500

    connection fixes

--- rv_tester.sv
+++ rv_tester.sv
@@ -561,6 +561,7 @@ module rv_tester
             .dut_clk(dut_clk[AXI_CLK_IDX]),
             .dut_reset(dut_reset[AXI_RESET_IDX]),
             .no_fetch(core_no_fetch[c]),
+            .event_trigger_vec(event_triggers[c]),
             `RV_TESTER_TRANSACTIONS_TRIGGERS_SOURCE_PORTS(2,c,0)
         );
     end
