f8f53e926ac447e93ec8be8052ded0ce810f411f
Pravin Tavagad <ptavagad@aus-rv-l-8.local.tenstorrent.com>
2024-08-02 14:30:37 -0500

    fix plusarg lint

--- sysmod/trickbox/imsic_driver.h
+++ sysmod/trickbox/imsic_driver.h
@@ -18,18 +18,8 @@
 #include "cvm/logger.hpp"
 #include "imsic_driver.h"
 #include "sysmod/trickbox/interrupter.h"
+#include "sysmod/sysmod_plusargs.h"
 
-DECLARE_int32(imsic_intr_delay_min);//, 4, "Minimum Delay between 2 consecutive interrupts");
-DECLARE_int32(imsic_intr_delay_max);//, 7, "Maximum Delay between 2 consecutive interrupts");
-DECLARE_bool(random_imsic_intr);//, false, "Drive random interrups");
-DECLARE_bool(disable_m_imsic_intr);
-DECLARE_bool(disable_s_imsic_intr);
-DECLARE_bool(disable_vs_imsic_intr);
-DECLARE_bool(disable_random_hart_imsic_intr);
-DECLARE_int32(imsic_intr_threshold);
-DECLARE_int32(imsic_vs_intr_threshold);
-DECLARE_int32(imsic_hart_threshold);
-DECLARE_int32(imsic_intr_start_delay);
 // Define a core local interruptor (imsic_driver) at the given address
 // and for the given hart count. The size will be 48k bytes.
 class imsic_driver : public subdevice
