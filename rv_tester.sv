24753bef2f4852d088be633b66622834778038fc
Sharanesh R <sr@tenstorrent.com>
2024-08-08 05:08:05 -0500

    Fixes around Debugger driving snippets

--- rv_tester.sv
+++ rv_tester.sv
@@ -128,7 +128,7 @@ module rv_tester
     bit print_terminate_message = '1;
 
     int dmi_poll_counter = 0; 
-    int dmi_poll_timeout = 8000;
+    int dmi_poll_timeout = 50000;
     logic dmi_poll_timeout_terminate;
     logic [31:0] dmi_commands_in_queue; 
 
@@ -144,8 +144,8 @@ module rv_tester
     string cvm_verbosity_string, gen_clocks_verbosity_string;
     int unsigned cvm_verbosity, gen_clocks_verbosity;
 
-    assign terminate           = (shutdown || rv_tester_error_terminate.terminate || ((sysmod_terminate.terminate || cosim_terminate_any || dmi_poll_timeout_terminate) && !sysmod_reset) || quiesce_counter > 0) && !rv_tester_reset|| tj_max_interrupt;
-    assign terminate_now       = (terminate && (quiesced || quiesce_counter >= quiesce_timeout) && (flush_complete || flush_counter >= flush_timeout) && ((dmi_commands_in_queue == '0) | (dmi_poll_counter > 'h1)) && (!trace_en || trace_quiesced || trace_counter >= trace_timeout) && (!jtag_en || jtag_quiesced )) || shutdown; 
+    assign terminate           = (shutdown || tj_max_interrupt || rv_tester_error_terminate.terminate || ((sysmod_terminate.terminate || cosim_terminate_any || dmi_poll_timeout_terminate) && !sysmod_reset) || quiesce_counter > 0) && !rv_tester_reset;
+    assign terminate_now       = (terminate && (quiesced || quiesce_counter >= quiesce_timeout) && (flush_complete || flush_counter >= flush_timeout) && ((dmi_commands_in_queue == '0) | (dmi_poll_counter > 'h1)) && (!trace_en || trace_quiesced || trace_counter >= trace_timeout) && (!jtag_en || jtag_quiesced )) || shutdown || tj_max_interrupt; 
 
     
     assign rerun_now           = terminated && num_reruns > 0;
@@ -454,7 +454,7 @@ module rv_tester
                 dmi_poll_timeout_terminate <= 1;
             end
             else if ((dmi_poll_counter >= 'h1) && terminate) begin
-               $display("<%0d> [RVTESTER]: Debug poll stopped as test pass condition detected limit reached", clocks); 
+               $display("<%0d> [RVTESTER]: Debug poll stopped as terminate condition detected", clocks); 
             end
         end
     end
