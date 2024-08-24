69cc873102bdf14f1297f6282afc4c11084794c9
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2024-08-24 15:43:48 -0500

    Added whisperNmi method to send a non-maskable-interrupt to whisper.

--- cosim/whisper_if/whisper_client.h
+++ cosim/whisper_if/whisper_client.h
@@ -59,6 +59,8 @@ class whisperClient {
     bool whisperPeekFpr(int hart, uint64_t addr, uint64_t& value);
     bool whisperPeekVpr(int hart, uint64_t addr, std::array<std::uint8_t, 32>&  value);
 
+    // Deliver a non-maskable interrupt to whisper.
+    bool whisperNmi(int hart, uint64_t time, uint64_t cause);
 
   private:
 
