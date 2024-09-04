e2d9105e89fef2583b1678327ecb5ce6bd78e9bd
Manav Chokshi <mchokshi@aus-rv-l-2.local.tenstorrent.com>
2024-09-04 00:42:33 -0500

    Adding mvip in cosim_resynch

--- cosim/bridge/bridge.cpp
+++ cosim/bridge/bridge.cpp
@@ -142,7 +142,7 @@ bridge::bridge(int num_harts, int xlen, int vlen, cvm::topology::loc_t loc, unsi
       "menvcfg","senvcfg","henvcfg", // FIXME: pointer masking change
       "pma","pmp", // FIXME: Performant NC change
       "vtype", // Permanent: Vector vtype will not be implemented
-      "mip","hip","vsip","hvip","sip","mireg","sireg","vsireg","mtopei","stopei","vstopei", // Permanent: Interrupts
+      "mip", "mvip", "hip","vsip","hvip","sip","mireg","sireg","vsireg","mtopei","stopei","vstopei", // Permanent: Interrupts
       "mtopi", "stopi", "vstopi", // RVTOOLS-3189
       "hpmcounter","hpmevent","scountovf","mcycle","minstret","minstreth", // Permanent: PMC events
       "dcsr","dpc","dscratch0", "dscratch1" // Permanent: Debug events
