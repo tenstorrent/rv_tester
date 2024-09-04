0931cc854646e31ef3c7ab5db52eea63f972cd3d
Bala Dhinesh M <baladhineshm@tenstorrent.com>
2024-09-04 07:14:29 +0000

    disable vxsat

--- cosim/bridge/bridge.cpp
+++ cosim/bridge/bridge.cpp
@@ -137,6 +137,7 @@ bridge::bridge(int num_harts, int xlen, int vlen, cvm::topology::loc_t loc, unsi
       //"htval","mtval2", // RVDE-10043
       "mtinst","htinst", // RVDE-10005
       "sstatus","mstatus","hstatus","mie","hie","vsie","sie", // RVDE-11840
+      "vxsat", // Vectors
       "tselect","tdata1","tdata2","tdata3","mcontext","tinfo", // Unimplemented: RVDE-7518, RVTOOLS-3124
       "fflags","fcsr", // Unimplemented
       "menvcfg","senvcfg","henvcfg", // FIXME: pointer masking change
