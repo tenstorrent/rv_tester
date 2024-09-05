eed61cc30a7aa6b1c14b223a0ca277461b9cb2a1
Pravin Tavagad <ptavagad@aus-rv-l-8.local.tenstorrent.com>
2024-09-05 06:32:18 -0500

    fix logging error

--- sysmod/trickbox/io_coh_helper.cpp
+++ sysmod/trickbox/io_coh_helper.cpp
@@ -155,7 +155,7 @@ void io_coh_helper::overlay_write(uint64_t addr,uint64_t data) {
 
 
 void io_coh_helper::overlay_read(uint64_t addr) {
-  cvm::log(cvm::FULL, "[io_coh_helper] axi read addr= {:#X} id = {} length = {}  \n",addr);
+  cvm::log(cvm::FULL, "[io_coh_helper] axi read addr= {:#X}   \n",addr);
    transactor::read_t r ;
    r.addr = addr;
    r.length = 0x40;
