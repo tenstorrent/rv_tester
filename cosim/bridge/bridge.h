98697779d5a66565a82e7c9e70e14c1f6a606797
Manav Chokshi <mchokshi@tenstorrent.com>
2024-11-05 11:02:30 +0000

    Reverting xtopei value printing for interrupted instruction due to cause 9, 11.

--- cosim/bridge/bridge.h
+++ cosim/bridge/bridge.h
@@ -93,7 +93,6 @@ public:
   void report_metrics();
   void process(const rv_tester::terminate_called &);
   void set_patch_mode(int patch) { patch_mode_ = static_cast<patch_mode> (patch); }
-  uint64_t get_csr_p(hart_id_t hart, src_t src, uint64_t addr) { return get_csr(hart, src, addr); }
 
 private:
 
