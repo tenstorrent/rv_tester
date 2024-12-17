38762ff35e12fb5db3b1d3d008e707c466e52a15
Pravin Tavagad <ptavagad@tenstorrent.com>
2024-12-17 15:29:50 +0000

    Call function to program resetvector

--- pwrmgmt/reset_sequence.cpp
+++ pwrmgmt/reset_sequence.cpp
@@ -226,6 +226,7 @@ cvm::messenger::task<void> reset_sequence::cpl_reset_sequence(rst_t rst_type) {
   } else if (FLAGS_patch_ram_check) {
     co_await patch_ram_check();
   };
+  co_await program_fe_resetvector();
   co_await release_cpl_nofetch();
   co_return;
 }
@@ -256,7 +257,7 @@ cvm::messenger::task<void> reset_sequence::check_pll_status() {
 cvm::messenger::task<void> reset_sequence::program_fe_resetvector() {
   co_await tick();
   co_await write(core_resetvector_mmr, 8, FLAGS_resetpc );
-
+  co_await tick();
   co_return;
 }
 
