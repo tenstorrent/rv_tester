1b0703b8e7ed4693e8d3e4397b5b7360a5b09cc9
Ajay Venkata Reddy <areddy@tenstorrent.com>
2025-01-14 23:01:52 -0600

    typo after refactor

--- cosim/bridge/bridge.cpp
+++ cosim/bridge/bridge.cpp
@@ -1869,7 +1869,7 @@ bool bridge::resynch_on_instr(const std::string& instr, const uint64_t& cycle) {
     bridge_log_(cvm::MEDIUM, "<{}> Resynch: Reason=[unsupported_csr_access]\n", cycle);
     return true;
   }
-  if (FLAGS_cosim_resynch_instr == "") {
+  if (FLAGS_cosim_resynch_instr != "") {
     std::stringstream ss(FLAGS_cosim_resynch_instr);
     while(ss.good()) {
       std::string s;
