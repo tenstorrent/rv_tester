25c8b6cd7be902470aff787fd46195f568697003
Ajay Venkata Reddy <areddy@tenstorrent.com>
2025-01-24 00:57:39 -0600

    Revert "JTAG clock changed from Fabric clock to Ref clock"
    This reverts commit 669bbcc54c251472ff2f033d2b265a47e1d4f181.

--- jtag_driver/jtag_driver.sv
+++ jtag_driver/jtag_driver.sv
@@ -132,9 +132,9 @@ import rv_tester_params::*;
   end
 
   // m_jtag_driver_tick
-  assign m_jtag_driver_ticks[0].valid = ~dut_reset & ((dut_clocks % 1600) == 0);
+  assign m_jtag_driver_ticks[0].valid = ~dut_reset & ((dut_clocks % 200) == 0);
   assign m_jtag_driver_ticks[0].data.location = location;
-  assign m_jtag_driver_ticks[0].data.cycle = jtag_socket_en?((jtag_socket_start | jtag_socket_end) ? dut_clocks : '0):1600;
+  assign m_jtag_driver_ticks[0].data.cycle = jtag_socket_en?((jtag_socket_start | jtag_socket_end) ? dut_clocks : '0):200;
   
   assign jtag_rdatas[0].valid         = read_data_valid_reg;
   assign jtag_rdatas[0].data.location = location;
@@ -213,7 +213,7 @@ typedef enum logic [1:0] {
   assign pos_tdo_en= ~jtag_resp.tdo_en;
 
   assign jtag_tck_trst.tck = clk;
-  assign jtag_tck_trst.trst = reset & ~((dut_clocks > 30) && (dut_clocks <100));
+  assign jtag_tck_trst.trst = reset & ~((dut_clocks > 30) && (dut_clocks <40));
 
 assign jtag_enable_begin = jtag_enable_begin_cpp ^ jtag_enable_begin_sv;
 
