0efc1a2a22befb092ebf6416973905b7185d17eb
baladhineshm <baladhineshm@tenstorrent.com>
2025-01-24 07:09:51 -0600

    fix fflag check for any cracked uop

--- cosim/bridge_if/bridge_if.h
+++ cosim/bridge_if/bridge_if.h
@@ -209,6 +209,7 @@ typedef struct rv_instr_s {
   std::string disasm = std::string(128, ' ');
   uint64_t uop = 0;
   bool vec_cracked = false;
+  bool cracked = false;
   bool csr_renamed = false;
   std::string csr_renamed_name = "";
   bool trap = false;
