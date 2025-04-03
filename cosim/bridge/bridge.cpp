63f22a61a167e9ce0e630cd82fa6b9ec5349a311
Sathvik Reddy Arrabothu <srarrabothu@rv-l-07.aus2.tenstorrent.com>
2025-04-03 05:19:27 -0500

    Poking  instr towhisper when  we enter DM through DCSR.ebreak

--- cosim/bridge/bridge.cpp
+++ cosim/bridge/bridge.cpp
@@ -594,6 +594,8 @@ void bridge::process_dut_instr_retire(hart_id_t hart, rv_instr_t& d) {
     IF_DEBUG("dut single step excp cause=31");
     return;
   }
+  
+  check_debug_mode_entry_via_ebreak(d);
 
   // Handle pre-step condition - Debug
   if (debug_mode_) {
@@ -885,17 +887,21 @@ void bridge::post_step_debug_poke(hart_id_t hart, const rv_instr_t& instr) {
   }
 }
 
+void bridge::check_debug_mode_entry_via_ebreak(const rv_instr_t& instr) {
+  dtvec_ebreak = false;
+  for (auto& csr : instr.csr) {
+    if (csr.csr_addr == c_dtvec_spec_csr && (csr.csr_wdata & 0x10)==0) {
+      dtvec_ebreak = true;
+      debug_mode_ = true;
+      break;
+    }
+  }
+}
+
 void bridge::pre_step_debug_poke(hart_id_t hart, const rv_instr_t& instr) {
   print(cvm::MEDIUM, "Debug pre step poking instruction in Debug mode\n", hart);
   bool valid;
   uint32_t opcode;
-  bool dtvec_ebreak = false;
-  for (auto& csr : instr.csr) {
-    if (csr.csr_addr == c_dtvec_spec_csr && (csr.csr_wdata & 0x10)==0) {
-      dtvec_ebreak = true;
-      break;
-    }
-  }
   if (instr.pc.pc_rdata == FLAGS_debug_exit_pc) {
     opcode = opcode_nop;
   }
