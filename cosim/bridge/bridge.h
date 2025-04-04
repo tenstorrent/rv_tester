a1dfb213f84c687c8e21912fee97095e90d0a38f
Varun Koyyalagunta <vkoyyalagunta@tenstorrent.com>
2025-04-04 19:07:29 +0000

    Revert "branch 'merge/master/srarrabothu-20250403052024' into 'master'"
    This reverts merge request !2321

--- cosim/bridge/bridge.h
+++ cosim/bridge/bridge.h
@@ -236,7 +236,6 @@ private:
   void pre_step_exception_poke(hart_id_t hart, const rv_instr_t& d);
   void pre_step_lrsc_poke(       hart_id_t hart, const rv_instr_t& d);
   void pre_step_debug_poke(      hart_id_t hart, const rv_instr_t& d);
-  void check_debug_mode_entry_via_ebreak(const rv_instr_t& d);
   void post_step_debug_poke(      hart_id_t hart, const rv_instr_t& d);
   void pre_step_nmi_poke(  hart_id_t hart, const rv_instr_t& d,       whisper_state_t& w);
   void pre_step_interrupt_poke(  hart_id_t hart, const rv_instr_t& d,       whisper_state_t& w);
@@ -379,7 +378,6 @@ private:
   bool ecall_ = false;
   bool is_priv_debug_mode_ = false;
   bool debug_mode_ = false;
-  bool dtvec_ebreak_ = false;
   bool debug_haltreq_asserted = false;
   bool excp_in_debug_mode = false;
   bool lrsc_fail_ = false;
