03b6c47ff5a69a9b97bdf4b76ae1525bdfe76e38
Hitesh Oleti <holeti@tenstorrent.com>
2025-04-17 04:47:40 +0000

    Removed cvm::ERROR for AXI Transactor ID queue overflow since it's being handled in sequences

--- transactors/axi_sw/axi_sw_mst.cpp
+++ transactors/axi_sw/axi_sw_mst.cpp
@@ -222,7 +222,7 @@ axi_sw_mst<B, R, ARQ, AWQ, WQ>::a_wrapper(uint64_t req_addr, size_t req_length, 
 
 
     if (!next_id(a.id)) {
-        cvm::log(cvm::ERROR, "[{}] Error: No free id's remaining for axi master\n", name_);
+        cvm::log(cvm::NONE, "[{}] No free id's remaining for axi master\n", name_);
         return false;
     }
 
@@ -247,7 +247,7 @@ axi_sw_mst<B, R, ARQ, AWQ, WQ>::push_a_no_id(const bool& aw, const axi::a_no_id_
     a.w = aw;
 
     if (!next_id(id)) {
-        cvm::log(cvm::NONE, "[{}] Error: No free id's remaining for axi master\n", name_);
+        cvm::log(cvm::NONE, "[{}] No free id's remaining for axi master\n", name_);
         return false;
     }
     a.id = id;
