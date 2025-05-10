f198a69d5c64c8d6338a79a734d50ac314a90f25
Devansh Sharma <dsharma@rv-l-11.aus2.tenstorrent.com>
2025-05-10 04:11:10 -0500

    updated jtag_driver_reset_fix

--- rv_tester.sv
+++ rv_tester.sv
@@ -903,7 +903,8 @@ module rv_tester
         )jtag_driver
         (
             .clk(dut_clk[REF_CLK_IDX]),
-            .reset(reset[COLD_RESET_IDX]),
+            /*.reset(reset[COLD_RESET_IDX]),*/
+            .reset(sys_reset[REF_CLK_IDX]), /*for both warm and cold reset*/
             .dut_clk(dut_clk[TB_CLK_IDX]),
             .dut_reset(dut_reset[TB_CLK_IDX]),
             .no_fetch(core_no_fetch[0]),
