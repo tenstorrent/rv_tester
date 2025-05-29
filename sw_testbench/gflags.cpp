a47c8170dcdb4bf2a1bb95bb522fec9549c3b509
Mansoor Anees <manees@tenstorrent.com>
2025-05-29 12:38:01 -0500

    Remove vip plusargs from sw_testbench

--- sw_testbench/gflags.cpp
+++ sw_testbench/gflags.cpp
@@ -8,5 +8,3 @@ DEFINE_string(cg_coverage_control, "", "command line flag : cg_coverage_control"
 DEFINE_string(covg_disable_cg, "", "command line flag : covg_disable_cg");
 DEFINE_int64(vcd_cycle_on, 0, "vcd_cycle_on");
 DEFINE_int64(vcd_cycle_off, 0, "vcd_cycle_off");
-DEFINE_bool(vip, false, "Set for vip builds");
-DEFINE_bool(vip_axi_dpi, false, "C++ vip <-> DPI <-> SV ports <-> DPI <-> C++ axi or C++ vip <-> C++ axi");
