47e2313495b553283cf02218c1b2420cc0005e07
Ajay Venkata Reddy <areddy@tenstorrent.com>
2025-06-26 17:42:31 +0000

    SEP fifo new implementation

--- sysmod/sep_entropy_fifo/sep_entropy_fifo.cpp
+++ sysmod/sep_entropy_fifo/sep_entropy_fifo.cpp
@@ -4,8 +4,9 @@
 
 DEFINE_uint32(entropy_fifo_size, 8, "Size of the entropy FIFO");
 sep_entropy_fifo::sep_entropy_fifo(const std::string& tag, uint64_t addr, unsigned, cvm::topology::loc_t loc)
-    : device(tag, addr, 0xc000, loc, &sep_entropy_fifo::write, &sep_entropy_fifo::read, this){
+    : device(tag, addr, 0xc000, loc, &sep_entropy_fifo::write, &sep_entropy_fifo::read, this) {
   cvm::log(cvm::HIGH, "sep_entropy_fifo created with size {}\n", FLAGS_entropy_fifo_size);
+  fill_fifo();
 }
 
 void
@@ -15,26 +16,28 @@ sep_entropy_fifo::read(const transactor::read_t& r, data_t& data) {
   auto length = r.length;
   uint64_t fifo_base = addr();
   uint64_t fifo_size = size();
-  if ((pa+length) > (fifo_base+fifo_size)) {
-    cvm::log(cvm::ERROR, "Error: Read outside of device range for sep_entropy_fifo, addr={:x} size={}\n", pa, length);
-    return;
-  } else if (length > FLAGS_entropy_fifo_size) {
-    cvm::log(cvm::ERROR, "Error: Reads with length greater than {} are not supported for sep_entropy_fifo\n", FLAGS_entropy_fifo_size);
-    return;
-  }
-  while (fifo_.size() < FLAGS_entropy_fifo_size)
-    fifo_.push(rng1());
-
   uint64_t dword = 0;
-  for (size_t i=0; i<length; i++) {
-    if (pa+i == fifo_base) {
-      dword |= FLAGS_entropy_fifo_size << (i*8);
-    } else if (pa+i < fifo_base+8) {
-        // do nothing (return zero)
-    } else {
-      dword |= uint64_t(fifo_.front()) << (i*8);
+  if (pa == fifo_base) {
+    dword = fifo_.size();
+    if (!fifo_.size()) fill_fifo();
+  } else {
+    if ((pa+length) > (fifo_base+fifo_size)) {
+      cvm::log(cvm::ERROR, "Error: Read outside of device range for sep_entropy_fifo, addr={:x} size={}\n", pa, length);
+      return;
+    }
+    uint32_t aligned_pa = pa & 0x7;
+    if (fifo_.empty()) {
+      fill_fifo();
+    } else if (aligned_pa < 4) {
+      dword = fifo_.front() >> aligned_pa;
       fifo_.pop();
     }
   }
   serializeInt(dword, length, data);
+}
+
+void
+sep_entropy_fifo::fill_fifo() {
+  while (fifo_.size() < FLAGS_entropy_fifo_size)
+   fifo_.push(rng1());
 }
