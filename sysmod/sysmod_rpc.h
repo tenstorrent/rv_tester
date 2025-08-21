8475726bec0b25abe9b83bfad3f2c6a2d212b484
Abdultaiyeb V <abduv@ausc-rv-l-13.local.tenstorrent.com>
2025-08-21 03:58:43 -0500

    adds two new rpc to sysmod, moves out set_csr/mmr

--- sysmod/sysmod_rpc.h
+++ sysmod/sysmod_rpc.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "cvm/messenger.hpp"
+#include "device.h"
 CVM_MESSENGER_procedure_call(sysmod_eot, void (uint64_t, size_t, device::data_t& data_t)); // sysmod - eot interface
 CVM_MESSENGER_procedure_call(sysmod_secure_mem, void (uint64_t& , uint64_t&)); // RPC to get secure region bounds
 CVM_MESSENGER_procedure_call(sysmod_backdoor_write, void (transactor::write_t&)); // sysmod - snoop_gen interface
+CVM_MESSENGER_procedure_call(sysmod_backdoor_write_boot, void (uint64_t, uint32_t, const device::data_t&, const device::strb_t&)); // sysmod_core_agent - sysmod interface
+CVM_MESSENGER_procedure_call(sysmod_get_boot_addr, uint64_t ()); // sysmod_core_agent - sysmod interface
