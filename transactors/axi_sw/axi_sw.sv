08d53b6ca0e51025b1926b26dcb53d94f6b3473e
Saurabh Singh <saurabhsingh@rv-l-09.aus2.tenstorrent.com>
2025-08-24 23:49:02 -0500

    Resolve MR-2998 thread.

--- transactors/axi_sw/axi_sw.sv
+++ transactors/axi_sw/axi_sw.sv
@@ -262,9 +262,7 @@ module axi_sw #(
       int unsigned lfsr_mask_``tx = '0;                                              \
       int unsigned lfsr_out_``tx;                                                    \
       logic tx``_ready_random;                                                       \
-      always_comb begin                                                              \
-        tx``_ready_random = (lfsr_out_``tx & lfsr_mask_``tx``) == 32'd0;             \
-      end                                                                            \
+      assign tx``_ready_random = (lfsr_out_``tx & lfsr_mask_``tx``) == 32'd0;        \
       cvm_lfsr #(                                                                    \
           .WIDTH(32'd32),                                                            \
           .NUM_TAPS(32'd4),                                                          \
