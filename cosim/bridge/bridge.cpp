c4b14734951e93c6ec12ee2a6aa41d8d5bc7f1b2
Sai Rajat Goparaju <sgoparaju@tenstorrent.com>
2025-09-29 13:30:41 -0500

    RVDE-26851 : Cache flag in mread packets

--- cosim/bridge/bridge.cpp
+++ cosim/bridge/bridge.cpp
@@ -2338,7 +2338,7 @@ void bridge::resynch(hart_id_t hart, const rv_instr_group_t& d) {
 }
 
 // Process mem accesses - load resolves
-void bridge::process_dut_mcm_read(hart_id_t hart, mem_t& m) {
+void bridge::process_dut_mcm_read(hart_id_t hart, mem_t& m, bool cache) {
   if (end_mcm_)
     return;
 
@@ -2372,12 +2372,12 @@ void bridge::process_dut_mcm_read(hart_id_t hart, mem_t& m) {
 
   if (m.v_ext){
     std::vector<uint64_t> data_vec = create_dword_vec(m.data_vec);
-    if ((!cvm::registry::messenger.call<whisperClient<uint64_t>::whisperMcmVecReadRPC>(cvm::topology::get_from_hierarchy("TOP.PLATFORM.WHISPER_CLIENT", 0), hart, m.cycle, m.tag, m.pa, m.size, data_vec, m.elem_idx, m.field, valid)|| !valid) && FLAGS_whisper_client_check) {
+    if ((!cvm::registry::messenger.call<whisperClient<uint64_t>::whisperMcmVecReadRPC>(cvm::topology::get_from_hierarchy("TOP.PLATFORM.WHISPER_CLIENT", 0), hart, m.cycle, m.tag, m.pa, m.size, data_vec, m.elem_idx, m.field, cache, valid)|| !valid) && FLAGS_whisper_client_check) {
       error("Hart {}: Failed mcm vec load\n", hart);
       return;
     }
   } else {
-    if ((!cvm::registry::messenger.call<whisperClient<uint64_t>::whisperMcmReadRPC>(cvm::topology::get_from_hierarchy("TOP.PLATFORM.WHISPER_CLIENT", 0), hart, m.cycle, m.tag, m.pa, m.size, m.data, m.elem_idx, m.field, valid)|| !valid) && FLAGS_whisper_client_check) {
+    if ((!cvm::registry::messenger.call<whisperClient<uint64_t>::whisperMcmReadRPC>(cvm::topology::get_from_hierarchy("TOP.PLATFORM.WHISPER_CLIENT", 0), hart, m.cycle, m.tag, m.pa, m.size, m.data, m.elem_idx, m.field, cache, valid)|| !valid) && FLAGS_whisper_client_check) {
       error("Hart {}: Failed mcm load\n", hart);
       return;
     }
--- cosim/bridge/bridge.h
+++ cosim/bridge/bridge.h
@@ -63,7 +63,7 @@ public:
   //   - Read (Ld completion)
   //   - Insert (St merge buffer insertion)
   //   - Write (St cache write)
-  virtual void process_dut_mcm_read(hart_id_t hart, mem_t& m) override;
+  virtual void process_dut_mcm_read(hart_id_t hart, mem_t& m, bool cache) override;
   virtual void process_dut_mcm_insert(hart_id_t hart, mem_t& m) override;
   virtual void process_dut_mcm_bypass(hart_id_t hart, mem_t& m, bool cache) override;
   virtual void process_dut_mcm_write(hart_id_t hart, mem_cl_t& m) override;
