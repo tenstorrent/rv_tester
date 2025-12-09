1a67f42e2a5505e6a915d250aeb7603ab3ab8d3a
ssingla <ssingla@tenstorrent.com>
2025-12-08 22:59:09 -0600

    remove separate patch mode from nmi_sequence

--- interrupts/external_interrupt_sequence.cpp
+++ interrupts/external_interrupt_sequence.cpp
@@ -94,7 +94,7 @@ void external_interrupt_sequence::drive_interrupt(){
   bool is_vgein_intr = false;
 	unsigned disable_flags = FLAGS_disable_m_imsic_intr |( FLAGS_disable_s_imsic_intr <<1) |( FLAGS_disable_vs_imsic_intr <<2);
   if(disable_flags == 0x7)
-	  cvm::log(cvm::`, "Error: [ExtInterruptSeq] Cant generate IMSIC interrupts when all interrupts are disabled \n");
+	  cvm::log(cvm::ERROR, "Error: [ExtInterruptSeq] Cant generate IMSIC interrupts when all interrupts are disabled \n");
 
 	do{
     intr_file = (rng1() % (3 )) ; //gen iter between 1 to max simul instr
