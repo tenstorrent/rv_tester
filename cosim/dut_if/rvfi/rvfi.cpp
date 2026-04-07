cfbd8a8e8066a635cc9358fb0dc9c16f8c5caf9d
travi <travi@tenstorrent.com>
2026-04-07 06:15:16 -0500

    linter fix

--- cosim/dut_if/rvfi/rvfi.cpp
+++ cosim/dut_if/rvfi/rvfi.cpp
@@ -24,7 +24,6 @@ DEFINE_bool(cache_model_en, false, "Enable MCM Cache Model");
 DEFINE_bool(emulate_amo_arithmetic, true, "Emulate amo arithmetic if dut harness does not provide amo outputs");
 DEFINE_uint64(debug_entry_pc, 0x42190800, "Debug Mode entry PC");
 DEFINE_uint64(debug_exit_pc, 0x421908cc, "Debug Mode exit PC");
-DECLARE_bool(debugrom);
 DEFINE_uint64(debug_mem_base, 0x42190000, "Debug Memory Base Address");
 DEFINE_uint64(debug_mem_size, 0x1000, "Debug Memory Size");
 DEFINE_bool(use_sw_priv, false, "Enable use of SW generation of priv/patch_mode values instead of hw");
